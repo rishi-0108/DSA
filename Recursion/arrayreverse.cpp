cd6cc66723e4e81ab3530cbacf6539dcb8765cd8
Rishi Vishwakarma <vish.rishi0108@gmail.com>
2026-01-02 00:54:22 +0530

    Revised recursion and prcaticed a program

--- Recursion/arrayreverse.cpp
+++ Recursion/arrayreverse.cpp
@@ -7,7 +7,22 @@ using namespace std;
 //     reverse(i+1,n-1,arr);
 // }
 
+int main() {
+    int arr[] = {10, 7, 8, 9, 1, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
 
+    cout << "Original Array: ";
+    for(int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
+    quickSort(arr, 0, n - 1);
+
+    cout << "\nSorted Array:   ";
+    for(int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
+    return 0;
+}
 
 int main(){
     int a[] =  {1,2,3,4,5};
