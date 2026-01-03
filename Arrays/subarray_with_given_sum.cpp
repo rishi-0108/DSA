0ad8e04f11a0f50b403d0da9be7c526df321a151
Rishi Vishwakarma <vish.rishi0108@gmail.com>
2026-01-03 12:07:21 +0530

    Started solving problem - Count the maximum subarray with given sum

---
+++ Arrays/subarray_with_given_sum.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int c=0;
+        for(int i=0;i<nums.size();i++){
+            for(int j=0;j<i;j++){
+                
+            }
+        }
+    }
+};
