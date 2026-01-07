567950cafc96734c957e40983859c0209b2d4274
Rishi Vishwakarma <vish.rishi0108@gmail.com>
2026-01-08 00:55:31 +0530

    Practiced a recursion problem

--- Recursion/arrayreverse.cpp
+++ Recursion/arrayreverse.cpp
@@ -7,22 +7,12 @@ using namespace std;
 //     reverse(i+1,n-1,arr);
 // }
 
-int main() {
-    int arr[] = {10, 7, 8, 9, 1, 5};
-    int n = sizeof(arr) / sizeof(arr[0]);
-
-    cout << "Original Array: ";
-    for(int i = 0; i < n; i++)
-        cout << arr[i] << " ";
-
-    quickSort(arr, 0, n - 1);
-
-    cout << "\nSorted Array:   ";
-    for(int i = 0; i < n; i++)
-        cout << arr[i] << " ";
-
-    return 0;
+void reverse(int i, int n , int arr[]){
+    if(i>n/2)return;
+    swap(arr[i],arr[n-i]);
+    reverse(i+1,n,arr);
 }
+
 
 int main(){
     int a[] =  {1,2,3,4,5};
