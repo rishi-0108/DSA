d0c3dd62e019f5ed4f4a35a602e243e573a155b3
Rishi Vishwakarma <vish.rishi0108@gmail.com>
2026-01-09 23:06:20 +0530

    Tried new problem of leader in matrix

--- Arrays/set_matrix_zero.cpp
+++ Arrays/set_matrix_zero.cpp
@@ -1,13 +1,33 @@
-#include <bits/stdc++.h>
+#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
-
-class Solution {
-public:
-     // Function to set entire row and column to 0 if an element in the matrix is 0
-    void setZeroes(vector<vector<int>>& matrix) {
-        // Get number of rows
-        int m = matrix.size();
-        // Get number of columns
-        int n = matrix[0].size();
+class solution{
+    public:
+    vector<int> leader(vector<int>& nums){
+        int rightmost=nums[nums.size()-1];
+        vector<int> res;
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i]>nums[i+1])
+            res.push_back(nums[i]);
+        }
+        res.push_back(rightmost);
+        return res;
     }
 };
+int main() {
+    vector<int> nums = {10, 22, 12, 3, 0, 6};
+    
+    // Create an instance of the Solution class
+    solution finder;
+    
+    // Get leaders using class method
+    vector<int> ans = finder.leader(nums);
+    
+    cout << "Leaders in the array are: ";
+    for (int i = 0; i < ans.size(); i++) {
+        cout << ans[i] << " ";
+    }
+    cout << endl;
+    
+    return 0;
+}
