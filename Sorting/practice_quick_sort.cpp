3392ed5b7836b15fafd74935a8a8545f0fda7637
Rishi Vishwakarma <vish.rishi0108@gmail.com>
2026-01-10 00:51:37 +0530

    Updated the final code for quick sort after minor bug fix

--- Sorting/practice_quick_sort.cpp
+++ Sorting/practice_quick_sort.cpp
@@ -27,35 +27,6 @@ void quickSort(int arr[], int low, int high) {
 
 // Driver code
 int main() {
-   #include <iostream>
-using namespace std;
-
-// Function to partition the array
-int partition(int arr[], int low, int high) {
-    int pivot = arr[high];   // Choosing last element as pivot
-    int i = low - 1;         // Index of smaller element
-
-    for(int j = low; j < high; j++) {
-        if(arr[j] <= pivot) {
-            i++;
-            swap(arr[i], arr[j]);
-        }
-    }
-    swap(arr[i + 1], arr[high]);  // Place pivot in correct position
-    return i + 1;
-}
-
-// Quick Sort function
-void quickSort(int arr[], int low, int high) {
-    if(low < high) {
-        int pi = partition(arr, low, high);
-        quickSort(arr, low, pi - 1);   // Left subarray
-        quickSort(arr, pi + 1, high);  // Right subarray
-    }
-}
-
-// Driver code
-int main() {
     int arr[] = {10, 7, 8, 9, 1, 5};
     int n = sizeof(arr) / sizeof(arr[0]);
 
@@ -70,6 +41,4 @@ int main() {
         cout << arr[i] << " ";
 
     return 0;
-}
-
 }
