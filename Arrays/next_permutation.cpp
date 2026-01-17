1c703e9cd7d84bd269c95c58032e9d704e3948e1
Rishi Vishwakarma <vish.rishi0108@gmail.com>
2026-01-17 21:58:09 +0530

    Solved the problem (Arrays) - Next Permutation

--- Arrays/next_permutation.cpp
+++ Arrays/next_permutation.cpp
@@ -3,8 +3,24 @@
 #include<climits>
 #include<bits/stdc++.h>
 using namespace std;
-class Soution{
-    vector<int> next_permutation(vector<int>& nums){
-        
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int ind=-1;
+        for(int i=nums.size()-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                ind=i;
+                break;
+            }
+        }
+        if(ind==-1){
+            reverse(nums.begin(),nums.end());
+        }
+        for(int i=nums.size()-1;i>ind;i--){
+            if(nums[i]>nums[ind]){
+                swap(nums[i],nums[ind]);
+            }
+        }
+        reverse(nums.begin()+ind+1,nums.end());
     }
 };
