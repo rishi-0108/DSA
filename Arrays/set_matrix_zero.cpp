92df73925591ccb8b110276f983baec9bd70e142
Rishi Vishwakarma <vish.rishi0108@gmail.com>
2026-01-18 19:36:17 +0530

    Added the drivers code after testing for various test cases

--- Arrays/set_matrix_zero.cpp
+++ Arrays/set_matrix_zero.cpp
@@ -33,3 +33,15 @@ public:
         }
     }
 };
+int main() {
+    Solution obj;
+    vector<vector<int>> matrix = {{0,1,2,0},{3,4,5,2},{1,3,1,5}};
+    obj.setZeroes(matrix);
+    for (auto row : matrix) {
+        for (auto val : row) {
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
