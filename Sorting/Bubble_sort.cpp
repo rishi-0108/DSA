c4bc8c0d11695ec0015e52bca5f6940c4edb4ae5
Rishi Vishwakarma <vish.rishi0108@gmail.com>
2026-01-24 19:42:57 +0530

    Revides Bubble sort

--- Sorting/Bubble_sort.cpp
+++ Sorting/Bubble_sort.cpp
@@ -7,12 +7,7 @@ void swap(int *i,int *j){
     *j=temp;
 }
 void bubble_sort(int arr[],int n){
-    for(int i=n-1;i>=0;i--){
-        for(int j=0;j<i;j++){
-            if(arr[j]>arr[j+1])swap(arr[j],arr[j+1]);
-        }
-    } 
-    for(int i=0;i<n;i++)cout<<arr[i]<<",";
+    
 }
 int main(){
     int n;
