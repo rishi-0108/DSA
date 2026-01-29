99a9ec5965a5c24746641c91c1ba8eda4f8bfc3c
Rishi Vishwakarma <vish.rishi0108@gmail.com>
2026-01-29 22:57:36 +0530

    Tested the optimal soution with different test cases

--- Arrays/majority_element_n3.cpp
+++ Arrays/majority_element_n3.cpp
@@ -62,25 +62,7 @@ public:
         }
         return res;
     }
-    vector<int> majorityElement_brute(vector<int>& nums) {
-        vector<int> res;
-        for(int i=0;i<nums.size();i++){
-            
-            if(  res.size()==0 || nums[i]!=res[0] ){
-                int c=0;
-
-                for(int j=0;j<nums.size();j++){
-                    if(nums[i]==nums[j])
-                    c++;
-                }
-            
-                if(c>(nums.size()/3))
-                    res.push_back(nums[i]);
-            }
-            if(res.size()==2)break;
-        }
-        return res;
-    }
+    
 };
 int main() {
     vector<int> arr = {11, 33, 33, 11, 33, 11};
