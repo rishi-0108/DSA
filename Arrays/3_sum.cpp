63df3a66bcbb888638479bae38ae5c6d1d67a856
Rishi Vishwakarma <vish.rishi0108@gmail.com>
2026-02-01 22:10:09 +0530

    Solved the brute force solution for - 3 Sum problem

--- Arrays/3_sum.cpp
+++ Arrays/3_sum.cpp
@@ -1,9 +1,21 @@
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
-class Solution{
-    int three_sum(vector<int>& nums){
-        int three_sum;
-        return three_sum;
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> threesum;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                for(int k=j+1;k<n;k++){
+                    int sum=nums[i]+nums[j]+nums[k];
+                    if(sum==0){
+                        threesum.push_back({nums[i],nums[j],nums[k]});
+                    }
+                }
+            }
+        }
+        return threesum;
     }
 };
