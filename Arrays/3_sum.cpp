6b5074e128e6394dcdd5ff375e176665c9b3b5b7
Rishi Vishwakarma <vish.rishi0108@gmail.com>
2026-02-03 23:41:09 +0530

    Brain stromed for a better solution for 3 sums problem

--- Arrays/3_sum.cpp
+++ Arrays/3_sum.cpp
@@ -3,6 +3,21 @@
 using namespace std;
 class Solution {
 public:
+    vector<vector<int>> threeSum_brute(vector<int>& nums) {
+        vector<vector<int>> threesum;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                for(int k=j+1;k<n;k++){
+                    int sum=nums[i]+nums[j]+nums[k];
+                    if(sum==0){
+                        threesum.push_back({nums[i],nums[j],nums[k]});
+                    }
+                }
+            }
+        }
+        return threesum;
+    }
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> threesum;
         int n=nums.size();
