8e70d2cd3e90874bc17c39457af1f001c9fe4b01
Rishi Vishwakarma <vish.rishi0108@gmail.com>
2026-02-04 21:17:07 +0530

    Fixed the logical error of the Brute force code of - 3 Sum

--- Arrays/3_sum.cpp
+++ Arrays/3_sum.cpp
@@ -1,22 +1,27 @@
 #include<iostream>
+#include<set>
 #include<bits/stdc++.h>
 using namespace std;
 class Solution {
 public:
     vector<vector<int>> threeSum_brute(vector<int>& nums) {
-        vector<vector<int>> threesum;
+        set<vector<int>> st;
         int n=nums.size();
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
                 for(int k=j+1;k<n;k++){
                     int sum=nums[i]+nums[j]+nums[k];
+                    vector<int> temp;
                     if(sum==0){
-                        threesum.push_back({nums[i],nums[j],nums[k]});
+                        temp={nums[i],nums[j],nums[k]};
+                        sort(temp.begin(),temp.end());
+                        st.insert(temp);
                     }
                 }
             }
         }
-        return threesum;
+        vector<vector<int>> ans(st.begin(),st.end());
+        return ans;
     }
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> threesum;
