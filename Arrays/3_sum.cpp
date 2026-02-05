ede28df2369a1be098cbd76fb930f4dabab30386
Rishi Vishwakarma <vish.rishi0108@gmail.com>
2026-02-05 21:41:36 +0530

    Solved the better solution of the 3 sums problem using hashinf comcept

--- Arrays/3_sum.cpp
+++ Arrays/3_sum.cpp
@@ -23,19 +23,33 @@ public:
         vector<vector<int>> ans(st.begin(),st.end());
         return ans;
     }
-    vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> threesum;
+    vector<vector<int>> threeSum_better(vector<int>& nums) {
+        set<vector<int>> ans;
         int n=nums.size();
         for(int i=0;i<n;i++){
+            set<int> hashset;
             for(int j=i+1;j<n;j++){
-                for(int k=j+1;k<n;k++){
-                    int sum=nums[i]+nums[j]+nums[k];
-                    if(sum==0){
-                        threesum.push_back({nums[i],nums[j],nums[k]});
-                    }
+                int req=-(nums[i]+nums[j]);
+                if(hashset.find(req) != hashset.end()){
+                    vector<int> temp={nums[i],nums[j],req};
+                    sort(temp.begin(),temp.end());
+                    ans.insert(temp);
                 }
+                hashset.insert(nums[j]);
             }
         }
-        return threesum;
+        return vector<vector<int>>(ans.begin(),ans.end());
     }
 };
+int main() {
+    vector<int> arr = {-1,0,1,2,-1,-4};
+    int n = arr.size();
+    Solution obj;
+    vector<vector<int>> res = obj.threeSum_better(arr);
+
+    for (auto &triplet : res) {
+        for (auto &num : triplet) cout << num << " ";
+        cout << endl;
+    }
+    return 0;
+}
