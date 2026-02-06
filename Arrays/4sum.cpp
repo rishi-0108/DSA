02d31962ca409415d1bbc9d5f4225d5dbe3e4e7f
Rishi Vishwakarma <vish.rishi0108@gmail.com>
2026-02-06 22:31:41 +0530

    Tried the brute force solution for 4 sum problem

---
+++ Arrays/4sum.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<set>
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<vector<int>> fourSum_brute(vector<int>& nums) {
+        set<vector<int>> st;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                for(int k=j+1;k<n;k++){
+                    int sum=nums[i]+nums[j]+nums[k];
+                    vector<int> temp;
+                    if(sum==0){
+                        temp={nums[i],nums[j],nums[k]};
+                        sort(temp.begin(),temp.end());
+                        st.insert(temp);
+                    }
+                }
+            }
+        }
+        vector<vector<int>> ans(st.begin(),st.end());
+        return ans;
+    }
+};
