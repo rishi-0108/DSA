fb3d2e28a2d82191a41e3723669aba0cc7ae3857
Rishi Vishwakarma <vish.rishi0108@gmail.com>
2026-02-07 19:21:57 +0530

    Solved the brute force solution for The 4 Sum problem

--- Arrays/4sum.cpp
+++ Arrays/4sum.cpp
@@ -5,22 +5,38 @@ using namespace std;
 class Solution {
 public:
     vector<vector<int>> fourSum_brute(vector<int>& nums) {
-        set<vector<int>> st;
-        int n=nums.size();
+        vector<vector<int>> ans;
+        int n= nums.size();
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
                 for(int k=j+1;k<n;k++){
-                    int sum=nums[i]+nums[j]+nums[k];
-                    vector<int> temp;
-                    if(sum==0){
-                        temp={nums[i],nums[j],nums[k]};
-                        sort(temp.begin(),temp.end());
-                        st.insert(temp);
+                    for(int l=k+1;l<n;l++){
+                        int sum=nums[i]+nums[j]+nums[k]+nums[l];
+                        if(sum==0){
+                            ans.push_back({nums[i], nums[j], nums[k], nums[l]});
+                        }
                     }
                 }
             }
         }
-        vector<vector<int>> ans(st.begin(),st.end());
         return ans;
     }
 };
+int main() {
+    // Input array
+    vector<int> arr = {1, 0, -1, 0, -2, 2};
+    // Target sum
+    int target = 0;
+
+    // Create object
+    Solution obj;
+    // Get all quadruplets
+    vector<vector<int>> ans = obj.fourSum_brute(arr);
+
+    // Print result
+    for (auto quad : ans) {
+        for (int num : quad) cout << num << " ";
+        cout << endl;
+    }
+    return 0;
+}
