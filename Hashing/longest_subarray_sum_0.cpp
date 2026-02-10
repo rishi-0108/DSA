758f3011b472e59d36adcec1301a06db8c13c533
Rishi Vishwakarma <vish.rishi0108@gmail.com>
2026-02-10 20:39:38 +0530

    Solved the optimal soultion - Largest subarray with sum 0

--- Hashing/longest_subarray_sum_0.cpp
+++ Hashing/longest_subarray_sum_0.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int solve(vector<int>& a) {
+int solve_brute(vector<int>& a) {
     int largest=0;
     int n=a.size();
     for(int i=0;i<n;i++){
@@ -18,10 +18,28 @@ int solve(vector<int>& a) {
     }
     return largest;
 }
+int solve(vector<int>& a) {
+    unordered_map<int,int> mp;
+    int sum=0;
+    int maxi=INT_MIN;
+    for(int i=0;i<a.size();i++){
+        sum+=a[i];
+
+        if(sum==0)
+            maxi=max(maxi,i+1);
+        else{
+            if(mp.find(sum) != mp.end())
+                maxi=max(maxi,i-mp[sum]);
+            else
+                mp[sum]=i;
+        }
+    }
+    return maxi;
+}
 
 int main() {
     // sample input
-    vector<int> a = {6, -2, 2, -8, 1, 7, 4, -10};
+    vector<int> a = {9, -3, 3, -1, 6, -5};
     // print result
     cout << solve(a) << endl;
     // exit
