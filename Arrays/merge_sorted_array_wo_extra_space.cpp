47a1ea93f90d67492d80795e7e00709e485987b7
Rishi Vishwakarma <vish.rishi0108@gmail.com>
2026-02-13 22:33:46 +0530

    Solved a partial solution for the problem - Merge sorted array without extra space (algorithm doesnt work for certain test cases)

---
+++ Arrays/merge_sorted_array_wo_extra_space.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<vector>
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+    public :
+    // vector<int> merge(vector<int>& a,vector<int>& b){
+    //     int n=a.size();
+    //     int m=b.size();
+    //     int i=0,j=0;
+    //     while(i<n && j<m){
+    //         if(a[i]==0){
+    //             swap(a[i],b[j]);
+    //             j++;
+    //             continue;
+    //         }
+    //         if(a[i]<=b[j]){
+    //             i++;
+    //         }
+    //         else{
+    //             swap(a[i],b[j]);
+    //             i++;
+    //         }
+
+    //     }
+    //     return a;
+    // }
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i=0,j=0;
+        while(i<m+n && j<n){
+            if(nums1[i]==0){
+                swap(nums1[i],nums2[j]);
+                j++;
+                continue;
+            }
+            if(nums1[i]<=nums2[j]){
+                i++;
+            }
+            else{
+                swap(nums1[i],nums2[j]);
+                i++;
+            }
+
+        }
+    }
+};
+int main() {
+    vector<int> nums1 = {1, 3, 5, 0, 0, 0};
+    vector<int> nums2 = {2, 4, 6};
+    int m = 3, n = 3;
+
+    Solution().merge(nums1,m,nums2,n);
+
+    // Print merged array
+    for (int num : nums1) cout << num << " ";
+    return 0;
+}
