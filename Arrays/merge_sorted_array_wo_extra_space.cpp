6d15af98b3b77f14e9977fb1507199407153fd83
Rishi Vishwakarma <vish.rishi0108@gmail.com>
2026-02-13 22:48:46 +0530

    Corrected the solution for - Merge sorted array without extra space

--- Arrays/merge_sorted_array_wo_extra_space.cpp
+++ Arrays/merge_sorted_array_wo_extra_space.cpp
@@ -4,28 +4,35 @@
 using namespace std;
 class Solution {
     public :
-    // vector<int> merge(vector<int>& a,vector<int>& b){
-    //     int n=a.size();
-    //     int m=b.size();
-    //     int i=0,j=0;
-    //     while(i<n && j<m){
-    //         if(a[i]==0){
-    //             swap(a[i],b[j]);
-    //             j++;
-    //             continue;
-    //         }
-    //         if(a[i]<=b[j]){
-    //             i++;
-    //         }
-    //         else{
-    //             swap(a[i],b[j]);
-    //             i++;
-    //         }
+    /*
+    
+    vector<int> merge(vector<int>& a,vector<int>& b){
+        int n=a.size();
+        int m=b.size();
+        int i=0,j=0;
+        while(i<n && j<m){
+            if(a[i]==0){
+                swap(a[i],b[j]);
+                j++;
+                continue;
+            }
+            if(a[i]<=b[j]){
+                i++;
+            }
+            else{
+                swap(a[i],b[j]);
+                i++;
+            }
 
-    //     }
-    //     return a;
-    // }
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        }
+        return a;
+    }
+        
+    */
+    void merge_initial_algo(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+
+        // doesnt work for certain test cases
+
         int i=0,j=0;
         while(i<m+n && j<n){
             if(nums1[i]==0){
@@ -41,6 +48,29 @@ class Solution {
                 i++;
             }
 
+        }
+    }
+
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i=m-1;
+        int j=n-1;
+        int k=m+n-1;
+        while(i>=0 && j>=0){
+            if(nums1[i]>=nums2[j]){
+                swap(nums1[i],nums1[k]);
+                k--;
+                i--;
+            }
+            else{
+                swap(nums2[j],nums1[k]);
+                k--;
+                j--;
+            }
+        }
+        while(k>=0){
+            swap(nums1[k],nums2[j]);
+            j--;
+            k--;
         }
     }
 };
