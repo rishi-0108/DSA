5f932932f106105ac4e0500d8324aa6edf8d65c6
Rishi Vishwakarma <vish.rishi0108@gmail.com>
2026-02-15 20:06:34 +0530

    Soved the optimal solution for - Maximum product in subaary

---
+++ Arrays/maximum_product_subarray.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+#include<climits>
+using namespace std;
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int pre=1,suf=1;
+        int maxi=INT_MIN;
+        for(int i=0;i<nums.size();i++){
+            if(suf==0)
+            suf=1;
+            if(pre==0)
+            pre=1;
+
+            pre*=nums[i];
+            suf*=nums[nums.size()-i-1];
+
+            maxi=max(maxi,max(pre,suf));
+        }
+        return maxi;
+    }
+};
