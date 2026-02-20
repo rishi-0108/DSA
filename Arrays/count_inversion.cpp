57eddd4ab8813a7a37d4e22825519ee0252044e3
Rishi Vishwakarma <vish.rishi0108@gmail.com>
2026-02-20 19:23:41 +0530

    Thought for possible solutions for-count inversion problem

---
+++ Arrays/count_inversion.cpp
@@ -0,0 +1,13 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function to count the number of inversions in an array
+// An inversion is a pair (i, j) such that i < j and arr[i] > arr[j]
+int numberOfInversions(vector<int>& a, int n) {
+    
+}
+
+int main() {
+   
+}
