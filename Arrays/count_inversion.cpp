a10ae748086c527fbb8b2e2d4069675680471a5d
Rishi Vishwakarma <vish.rishi0108@gmail.com>
2026-02-21 17:11:44 +0530

    Coded the brute force solution - Count inversion

--- Arrays/count_inversion.cpp
+++ Arrays/count_inversion.cpp
@@ -5,7 +5,14 @@ using namespace std;
 // Function to count the number of inversions in an array
 // An inversion is a pair (i, j) such that i < j and arr[i] > arr[j]
 int numberOfInversions(vector<int>& a, int n) {
-    
+    int cnt = 0; // Initialize inversion count
+    // Check all pairs
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (a[i] > a[j]) cnt++; // Increment count if inversion found
+        }
+    }
+    return cnt;
 }
 
 int main() {
